e0d124ed0ec76807003f86c7a84fa61c66b40b84
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2024-12-17 19:45:04 +0000

    1.02

--- src/videos/updateVideoController.ts
+++ src/videos/updateVideoController.ts
@@ -8,20 +8,7 @@ export const updateVideoController = (req: Request, res: Response) => {
             return res.status(404).json({ message: `No video with id ${req.params.id}` });
         }
         const errors = inputValidation(req.body);
-        if (req.body.hasOwnProperty("canBeDownloaded")) {
-            if (typeof req.body.canBeDownloaded !== 'boolean') {
-                if (req.body.canBeDownloaded === "true") {
-                    req.body.canBeDownloaded = true;
-                } else if (req.body.canBeDownloaded === "false") {
-                    req.body.canBeDownloaded = false;
-                } else {
-                    errors.errorsMessages.push({
-                        message: "canBeDownloaded must be a boolean or a string that can be converted to a boolean",
-                        field: "canBeDownloaded"
-                    });
-                }
-            }
-        }
+
         if (errors.errorsMessages.length > 0) {
             return res.status(400).send(errors);
         }
