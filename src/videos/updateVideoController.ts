39f42fb75266c7cbd35f92c6661dfab3013cb269
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2024-12-17 19:29:44 +0000

    1.02

--- src/videos/updateVideoController.ts
+++ src/videos/updateVideoController.ts
@@ -1,17 +1,29 @@
 import { db } from "../db/db";
 import { Request, Response } from "express";
 import { inputValidation } from "./createVideoController";
-import {video} from "../db/dataset";
 export const updateVideoController = (req: Request, res: Response) => {
     try {
         const foundVideo = db.videos.find((video) => video.id === +req.params.id);
         if (!foundVideo) {
             return res.status(404).json({ message: `No video with id ${req.params.id}` });
         }
+
+
+
+
         const errors = inputValidation(req.body);
+        if (typeof req.body.canBeDownloaded !== 'boolean') {
+            if (req.body.canBeDownloaded === "true") {
+                errors.errorsMessages.push({
+                    message: "canBeDownloaded must be a boolean or a string that can be converted to a boolean",
+                    field: "canBeDownloaded"
+                });
+            }
+        }
         if (errors.errorsMessages.length > 0) {
             return res.status(400).send(errors);
         }
+
         Object.assign(foundVideo, req.body);
         return res.sendStatus(204);
     } catch (error) {
