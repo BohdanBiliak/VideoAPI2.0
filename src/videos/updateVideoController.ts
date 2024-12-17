6b09a22443891e6d2bab54a10b3809b825309183
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2024-12-17 18:20:25 +0000

    1.02

--- src/videos/updateVideoController.ts
+++ src/videos/updateVideoController.ts
@@ -1,19 +1,21 @@
-import {db} from "../db/db";
-import {Request, Response} from "express";
-import {InputValidation} from "./createVideoController"
+import { db } from "../db/db";
+import { Request, Response } from "express";
+import { inputValidation } from "./createVideoController";
+
 export const updateVideoController = (req: Request, res: Response) => {
-    const foundVideo = db.videos.find((video) => video.id === +req.params.id);
-    if (!foundVideo) {
-        res.status(404).json({ message: 'No video with id ' + req.params.id });
-        return;
+    try {
+        const foundVideo = db.videos.find((video) => video.id === +req.params.id);
+        if (!foundVideo) {
+            return res.status(404).json({ message: `No video with id ${req.params.id}` });
+        }
+        const errors = inputValidation(req.body);
+        if (errors.errorsMessages.length > 0) {
+            return res.status(400).send(errors);
+        }
+        Object.assign(foundVideo, req.body);
+        return res.sendStatus(204);
+    } catch (error) {
+        console.error("Error updating video:", error);  // Log the error for debugging
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    const errors = InputValidation(req.body);
-    if (errors.errorsMessage.length > 0) {
-        res.status(400).send(errors);
-        return;
-    }
-
-    Object.assign(foundVideo, req.body);
-    res.sendStatus(204);
 };
